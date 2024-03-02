077c2a17f73c9a382f72a71c7c3c4ee6c3539099
Guilherme Esteves <guilhermeestevesps@hotmail.com>
2024-03-01 21:18:16 -0300

    Simplified 8h line of .py file to transform GET to json without using json library. JSON removed from import.

--- meme_flask.py
+++ meme_flask.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-import requests, json
+import requests
 
 app = Flask(__name__)
 
 def get_meme():
     url = "https://meme-api.com/gimme"
-    response = json.loads(requests.request("GET", url).text)
+    response = requests.get(url).json();
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
     return meme_large, subreddit
